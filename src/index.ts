b5b0dcd98387b1d46e984ab119264c6dcf4efe66
Anthony Sylvester <anthony.sylvester99@gmail.com>
2020-12-30 19:01:21 -0800

    adds user authentication and validation

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
 // import { Post } from './entities/Post';
 
 const main = async () => {
@@ -20,7 +21,7 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
